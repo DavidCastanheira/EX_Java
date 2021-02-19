4e030b6f67a5c992efac0204e75d4fab3d9186a5
David Castanheira <47610728+DvdCast231@users.noreply.github.com>
2021-02-19 12:25:02 -0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,12 @@
+import java.util.ArrayList;
+
+class Main {
+  public static void main(String[] args) {
+    Dicionario<String, Double> dic1 = new Dicionario<>();
+   
+    dic1.add("dsadsa", 22.2);
+
+    Par<Double, String> par = new Par();
+
+  }
+}
